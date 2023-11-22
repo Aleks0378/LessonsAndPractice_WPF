56f3fd1c26504c9fdb820274276926c7ebc595e0
Aleks0378 <aleksstepan0378@gmail.com>
2023-11-22 20:47:47 +0200

    Practice_22.11_Task_1

---
+++ Practice_20.11_Task_3/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+//Создайте приложение, в котором пользователь может создавать и управлять событиями на календаре.
+//Вам нужно внедрить следующую функциональность:
+ 
+//1) Ваше приложение должно содержать элемент управления Calendar для выбора даты.
+//Пользователь может выбрать дату, и выбранная дата будет выделена на календаре. 
+//2) Пользователь может добавлять события, связанные с выбранной датой.
+//Например, пользователь может выбрать дату и ввести название и описание события.
+//Эти события должны сохраняться в памяти приложения (коллекция).
+//3) События, связанные с выбранной датой, должны быть отображены под календарем.
+//Вы можете использовать ListView или DataGrid для отображения списка событий.
+
+namespace Practice_20._11_Task_3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Practice_22.11_Task_1/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+//Создайте приложение WPF, которое имеет две кнопки: "Открыть окно" и "Закрыть окно".
+//При нажатии на кнопку "Открыть окно" должно открываться новое окно с надписью "Привет, это новое окно!".
+//При нажатии на кнопку "Закрыть окно" это новое окно должно закрываться.
+
+namespace Practice_22._11_Task_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenwindowButton_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 window1 = new Window1();
+            window1.Show();
+        }
+    }
+}
